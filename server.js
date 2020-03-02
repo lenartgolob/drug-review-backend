7dcb614f15e347d614e994945bec38463a2216f0
lenci123 <lenart.golob17@gmail.com>
2020-03-02 17:14:06 +0100

    registration + hash gesla

--- server.js
+++ server.js
@@ -1,7 +1,15 @@
-const express = require('express');
-const mysql = require('mysql');
+var express = require('express');
+var cors = require('cors');
+var bodyParser = require("body-parser");
+var mysql = require('mysql');
+
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
 
 const app = express();
+app.use(cors())
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 const connection = mysql.createConnection({
     host: 'localhost',
@@ -41,6 +49,87 @@ app.get('/api/customers', (req, res) => {
   res.json(customers);
 });
 
+app.post('/session', (req, res) => {
+
+  var email = req.body.email;
+  var password = req.body.password;
+
+  // Checks if email is valid
+  var sql = "SELECT * FROM users WHERE email = ?";
+  connection.query(sql, email, (error, user, fields) =>{
+    if(!!error) {
+      console.log('Error in query');
+    } else {
+      console.log('Succesfull query \n');
+      if(user != ''){
+        console.log();
+        
+        bcrypt.compare(password, user[0].password, function(err, result) {
+          if (result == true) {
+            res.send(user);
+            res.redirect('/');
+          } 
+          else {
+          res.send('Incorrect password');
+          res.redirect('/');
+          }
+        });
+
+      }
+      else {
+        console.log("ne obsstaja")
+      }
+    }
+  });
+
+
+
+
+  console.log(email);
+  console.log(password);
+
+
+
+  // res.json();
+});
+
+app.post('/registration', (req, res) => {
+
+  var name = req.body.name;
+  var email = req.body.email;
+  var password = req.body.password;
+
+  bcrypt.hash(password, saltRounds, function(err, hash) {
+    console.log("Registration: " + hash)
+
+    var sql = "INSERT INTO users (name, email, password) VALUES (?, ?, ?);";
+    connection.query(sql, [name, email, hash], (error, rows, fields) =>{
+      if(!!error) {
+        console.log('Error in query');
+      } else {
+        console.log('Succesfull query \n');
+        if(rows != ''){
+          res.json(rows)
+        }
+        else {
+          console.log("crnc")
+        }
+        console.log(rows);
+      }
+    });
+
+  });
+
+
+
+  console.log(name);
+  console.log(email);
+  console.log(password);
+
+
+
+  // res.json();
+});
 const port = 5000;
 
 app.listen(port, () => `Server running on port ${port}`);
